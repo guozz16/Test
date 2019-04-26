3060f6c2e2f9ea98a92cc94363235e601e5dfc73
Guo Zizhen <guozz16@mails.tsinghua.edu.cn>
2019-04-26 18:21:53 +0800

    Import load & weather

--- test.py
+++ test.py
@@ -1,27 +1,28 @@
-import pypower.api as pp
-import networkx as nx  
-import matplotlib.pyplot as plt
-
-g = nx.Graph()
-
-case = pp.case39()
-gen_list = []
-bus_list = []
-for gen in case['gen']:
-	gen_list.append(int(gen[0]))
-
-for bus in case['bus']:
-	bus_list.append(int(bus[0]))
-
-for brch in case['branch']:
-	g.add_edge(int(brch[0]),int(brch[1]))
-
-pos = nx.kamada_kawai_layout(g)
-nx.draw_networkx_nodes(g,pos,nodelist=gen_list,node_color='g',node_size=500,alpha=0.8)
-nx.draw_networkx_nodes(g,pos,nodelist=list(set(bus_list).difference(set(gen_list))),
-	node_color='r',node_size=300,alpha=0.8)
-nx.draw_networkx_edges(g, pos, width=1.0, alpha=0.5)
-nx.draw_networkx_labels(g,pos)
-plt.axis('off')
-plt.show()
-plt.savefig("path.png")
+from test2 import day
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import numpy as np
+df=pd.read_excel("STLF_DATA_IN_1.xls",index_col=0,header=None,sheet_name="负荷数据")
+df2=pd.read_excel("STLF_DATA_IN_1.xls",index_col=0,header=None,sheet_name="气象数据")
+days=[]
+for i in range(len(df)):
+	d=df.index[i]
+	try:
+		w=dict(zip(df2.loc[d][1].values,df2.loc[d][2].values))
+	except KeyError: 
+		days.append(day(d,df.loc[d]))
+	else:
+		days.append(day(d,df.loc[d],w))
+print(days[-2].date)
+print(days[-2].load)
+print(days[-2].weather)
+# d=day(df.iloc[0,0],df.iloc[0,1:],df2.iloc[0,])
+# # b=set(df2[1])
+# # print(b)
+# print(d.date)
+# print(d.load)
+# print(d.weather)
+# print(a)
+# fig = plt.figure()
+# plt.plot(a)
+# plt.show()
--- test2.py
+++ test2.py
@@ -1,3 +1,11 @@
-from pypower.api import case30
-case = case30()
-print(case['gen'])
+import numpy as np
+class day:
+	def __init__(self,date,load,weather={}):
+		self,
+		self.date=date
+		self.load=np.array(load)
+		self.weather={}
+		self.weather.update(weather)
+		self.max_load=self.load.max()
+		self.min_load=self.load.min()
+		self.mean_load=self.load.mean()
